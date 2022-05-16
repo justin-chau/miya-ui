c002e26f6dc8c333596a410cbf90ec05167c685b
Justin Chau <justin.jay.chau@gmail.com>
2022-05-16 02:40:26 -0400

    Config Prettier and ESlint.

--- src/components/Button/Button.stories.tsx
+++ src/components/Button/Button.stories.tsx
@@ -1,14 +1,16 @@
+import { ComponentStory, ComponentMeta } from "@storybook/react";
 import React from "react";
+
 import Button from "./Button";
 
 export default {
   component: Button,
   title: "Button",
-};
+} as ComponentMeta<typeof Button>;
 
-const Template = (args) => <Button {...args} />;
+const Template: ComponentStory<typeof Button> = (args) => <Button {...args} />;
 
 export const Default = Template.bind({});
 Default.args = {
-  label: "Hello",
+  label: "Test",
 };
