e45e43c56fa61408ddc226f212f95d056e6ec3af
Justin Chau <justin.jay.chau@gmail.com>
2022-05-16 01:35:34 -0400

    Add npm github config.

---
+++ dist/cjs/index.js
@@ -0,0 +1,2 @@
+"use strict";function e(e){return e&&"object"==typeof e&&"default"in e?e:{default:e}}Object.defineProperty(exports,"__esModule",{value:!0});var t=e(require("react"));exports.Button=function(e){var r=e.label;return t.default.createElement("button",null,r)};
+//# sourceMappingURL=index.js.map
---
+++ dist/esm/index.js
@@ -0,0 +1,2 @@
+import r from"react";var t=function(t){var e=t.label;return r.createElement("button",null,e)};export{t as Button};
+//# sourceMappingURL=index.js.map
